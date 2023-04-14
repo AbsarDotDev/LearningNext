ae803d955fa00a1f057ca0322a480c7f33eb152c
Absaromatic <absarali701@gmail.com>
2023-04-15 02:27:39 +0500

    ContentFul Author Reference

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env:{
+    CONTENTFUL_SPACE_ID:'7evp6b5xbxo3',
+CONTENTFUL_ACCESS_KEY:'Atu2VZmgRDZWrD8FBzkTaCX8SzgtaFD3qW085hk-aHE'
+  },
   images: {
     remotePatterns: [
       {
